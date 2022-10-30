035d48b9025689fec80f643fa136f42af63de7af
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-10-30 16:15:03 +0700

    Refactor

--- transfer.ts
+++
@@ -1,7 +0,0 @@
-import { Address } from './models/Address'
-import { BigNumber } from 'zenbox-util/bignumber'
-import { task } from '../../src/task'
-
-export async function transfer(from: Address, to: Address, amount: BigNumber) {
-  throw task()
-}
