ad254e052e3d4e4f89b94a06f828951b23ec49c5
Denis Gorbachev <829578+DenisGorbachev@users.noreply.github.com>
2022-12-20 16:14:43 +0700

    Fairpool

--- models/AmountBN/sumAmountBNs.ts
+++ models/AmountBN/sumAmountBNs.ts
@@ -1,6 +1,6 @@
+import { sumBNs } from '../../../bn/utils'
 import { AmountBN } from '../AmountBN'
-import { sumBN } from '../../../bn/utils'
 
 export function sumAmountBNs(object: { amount: AmountBN }[]) {
-  return sumBN(object.map(o => o.amount))
+  return sumBNs(object.map(o => o.amount))
 }
